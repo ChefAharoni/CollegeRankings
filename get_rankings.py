2fbab77e9e465962dbe1f66b94559b4271d56c7b
ChefAharoni <ChefAharoni@proton.me>
2024-01-15 12:26:18 -0800

    Removed check for only US data; added a country code for the file name; added some csv examples

--- get_rankings.py
+++ get_rankings.py
@@ -8,45 +8,73 @@ import time
 
 
 def init_driver():
-    """Initialize the Selenium WebDriver."""
+    """
+    Initialize the Selenium WebDriver.
+
+    Returns:
+        driver (WebDriver): The initialized Selenium WebDriver.
+    """
     driver = webdriver.Chrome()
     return driver
 
 
-def select_us_filter(driver):
-    """Select the United States filter."""
+def select_us_filter(driver: webdriver, country="United States") -> None:
+    """
+    Select the United States filter.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver.
+
+    Returns:
+        None
+    """
     input_wrappers = driver.find_elements(By.CLASS_NAME, "inputWrapper")
     filter_button = input_wrappers[2]
     filter_button.click()
     time.sleep(2)  # Wait for the dropdown to open
-
-    us_option_xpath = "//li[contains(text(), 'United States')]"
+    country_option_xpath = f"//li[contains(text(), '{country}')]"
+    # us_option_xpath = "//li[contains(text(), 'United States')]"
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, us_option_xpath))
+        EC.element_to_be_clickable((By.XPATH, country_option_xpath))
     ).click()
     time.sleep(2)  # Wait for the filter to apply
 
 
-def scrape_page(driver):
-    """Scrape the required data from a single page."""
+def scrape_page(driver: webdriver) -> list[str, str]:
+    """
+    Scrape the required data from a single page.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver.
+
+    Returns:
+        data (list): A list of dictionaries containing the scraped data.
+    """
     soup = BeautifulSoup(driver.page_source, "html.parser")
     us_universities = soup.find_all("tr", {"data-v-ae1ab4a8": True})
     data = []
 
     for uni in us_universities:
-        if "us.png" in str(uni):
-            ranking_div = uni.find("div", class_="ranking")
-            ranking = ranking_div.get_text(strip=True) if ranking_div else None
-            name_span = uni.find("span", class_="univ-name")
-            name = name_span.get_text(strip=True) if name_span else None
-            if ranking and name:
-                data.append({"Ranking": ranking, "Name": name})
+        ranking_div = uni.find("div", class_="ranking")
+        ranking = ranking_div.get_text(strip=True) if ranking_div else None
+        name_span = uni.find("span", class_="univ-name")
+        name = name_span.get_text(strip=True) if name_span else None
+        if ranking and name:
+            data.append({"Ranking": ranking, "Name": name})
 
     return data
 
 
-def navigate_to_next_page(driver):
-    """Navigate to the next page if possible."""
+def navigate_to_next_page(driver: webdriver) -> bool:
+    """
+    Navigate to the next page if possible.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver.
+
+    Returns:
+        bool: True if successfully navigated to the next page, False otherwise.
+    """
     try:
         next_button = driver.find_element(By.CLASS_NAME, "ant-pagination-next")
         if "ant-pagination-disabled" in next_button.get_attribute("class"):
@@ -60,13 +88,38 @@ def navigate_to_next_page(driver):
         return False
 
 
-def main(url):
-    """Main function to orchestrate the scraping process."""
-    driver = init_driver()
-    driver.get(url)
+def get_country_code(country: str):
+    """
+    Returns the country code for a given country name.
+
+    Parameters:
+        country (str): The name of the country.
+
+    Returns:
+        str: The country code.
+    """
+    words = country.split()
+    if len(words) == 1:
+        return words[0].title()
+    else:
+        return "".join([w[0] for w in words]).upper()
+
+
+def main(url: str, country="United States") -> None:
+    """
+    Main function to orchestrate the scraping process.
+
+    Args:
+        url (str): The URL of the webpage to scrape.
+
+    Returns:
+        None
+    """
+    driver = init_driver()  # Initialize the Selenium WebDriver
+    driver.get(url)  # Navigate to the URL
     time.sleep(5)  # Wait for the page to fully load
 
-    select_us_filter(driver)  # Select the United States filter
+    select_us_filter(driver, country)  # Select the United States filter
 
     all_data = []
     while True:
@@ -78,11 +131,17 @@ def main(url):
 
     # Save the scraped data to CSV
     df = pd.DataFrame(all_data)
-    csv_file_path = "data/us_universities_rankings_all_pages.csv"
+    country_code = get_country_code(country)
+    csv_file_path = f"data/{country_code}_universities_rankings_all_pages.csv"
     df.to_csv(csv_file_path, index=False)
     print(f"Data scraped and saved to {csv_file_path}")
 
 
 if __name__ == "__main__":
     url = "https://www.shanghairanking.com/rankings/gras/2023/RS0210"
-    main(url)
+    countries = ["United States", "United Kingdom", "Canada", "Israel"]
+    # country = "United States"
+    # country = "United Kingdom"
+    # country = "Israel"
+    for country in countries:
+        main(url, country)
